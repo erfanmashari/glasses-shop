d3786003a039ece89c52ba35779ddda80c0286c8
sudo.developer <erfanmashari2@gmail.com>
2023-02-07 16:02:58 +0330

    Complete header of home page
    header of home page is completed

--- frontend/public/pages/index.js
+++ frontend/public/pages/index.js
@@ -1,10 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import HomeComponent from "../components/Home"
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <HomeComponent />
+    </>
   )
 }
