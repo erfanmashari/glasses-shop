ca5b7b5fb3b0092a3b4ae3da269ba888dc3334ff
sudo.developer <erfanmashari2@gmail.com>
2023-02-08 19:27:25 +0330

    Set api for similar products in product page
    api for similar products in product page is set

--- frontend/public/components/Product/SimilarProductsItem.jsx
+++ frontend/public/components/Product/SimilarProductsItem.jsx
@@ -1,14 +1,26 @@
 import Image from "next/image";
+import Link from "next/link";
 
-const SimilarProductsItem = () => {
+const SimilarProductsItem = ({ product }) => {
+  let imageSource = "";
+  for (const image of product.images) {
+    if (image.isCoverImage) {
+      imageSource = `http://localhost:8000/products/${image.fileName}.jpg`;
+      break;
+    }
+  }
+
   return (
-    <li className="w-1/5 h-full flex flex-col justify-start items-start gap-2 p-4">
+    <Link
+      href={`product/${product.nameFa}`}
+      className="w-1/5 h-full flex flex-col justify-start items-start cursor-pointer gap-2 p-4"
+    >
       <div className="w-full h-3/5 relative">
-        <Image fill={true} alt={"product image"} src={"/images/test.jpg"} />
+        <Image fill={true} alt={"product image"} src={imageSource} />
       </div>
-      <p className="font-bold">عینک آفتابی مارتیانو Martiano YD1050 C1</p>
-      <p className="text-sm">1,190,000 تومان</p>
-    </li>
+      <p className="font-bold">{product.nameFa}</p>
+      <p className="text-sm font-bold">{product.price} تومان</p>
+    </Link>
   );
 };
 
--- frontend/public/components/Product/SimilarProductsList.jsx
+++ frontend/public/components/Product/SimilarProductsList.jsx
@@ -1,6 +1,38 @@
 import SimilarProductsItem from "./SimilarProductsItem";
 
+import { useEffect } from "react";
+
+import axios from "axios";
+import { checkFetchResponse } from "../../functions";
+
+import { useSelector, useDispatch } from "react-redux";
+import { changeSimilarProductFromBackend } from "../../redux/actions/product";
+
 const SimilarProductsList = () => {
+  const dispatch = useDispatch();
+
+  // get discount products from redux/reducers/product/productInfo.js
+  const productInfo = useSelector((state) => state.productInfo);
+
+  // get discount products from redux/reducers/product/similarProducts.js
+  const similarProducts = useSelector((state) => state.similarProducts);
+
+  useEffect(() => {
+    // get discount products from backend
+    console.log("test: ", `${process.env.NEXT_PUBLIC_SERVER}products/${productInfo.category}`)
+    axios
+      .get(`${process.env.NEXT_PUBLIC_SERVER}products/${productInfo.category}`)
+      .then((response) => {
+        const res = checkFetchResponse(response);
+
+        if (res.ok) {
+          // set products in productsList reducer
+          dispatch(changeSimilarProductFromBackend(res.data.products));
+        }
+      });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div
       className="w-full flex flex-col justify-start items-start rounded-xl gap-4 p-8 mt-8"
@@ -8,11 +40,13 @@ const SimilarProductsList = () => {
     >
       <h2 className="text-xl font-bold">محصولات مشابه</h2>
       <ul className="w-full flex flex-row justify-start items-center bg-white rounded-lg" style={{ height: "240px" }}>
-        <SimilarProductsItem />
-        <SimilarProductsItem />
-        <SimilarProductsItem />
-        <SimilarProductsItem />
-        <SimilarProductsItem />
+        {similarProducts.map((product, index) => (
+          productInfo._id !== product._id && 
+          <SimilarProductsItem
+            key={index}
+            product={product}
+          />
+        ))}
       </ul>
     </div>
   );
