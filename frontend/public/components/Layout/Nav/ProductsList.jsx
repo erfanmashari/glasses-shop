1e7dde515fa0eb3135091992e9a518418f2c806a
sudo.developer <erfanmashari2@gmail.com>
2023-02-08 19:30:36 +0330

    Set link to home page for website logo

--- frontend/public/components/Layout/Nav/ProductsList.jsx
+++ frontend/public/components/Layout/Nav/ProductsList.jsx
@@ -1,20 +1,30 @@
 // import components
 import ProductsItem from "./ProductsItem";
 
-import Image from "next/image"
-
+import Image from "next/image";
+import Link from "next/link";
 
 const ProductsList = () => {
   return (
     <div className="flex flex-row justify-center items-center gap-2 py-3">
-      <Image width={60} height={60} alt="logo" src="/images/sudo-orange.png" />
-    <ul className={`flex flex-row justify-center items-center text-sm gap-4`} style={{ color: "#4b4b4b" }}>
+      <Link href={"/"}>
+        <Image
+          width={60}
+          height={60}
+          alt="logo"
+          src="/images/sudo-orange.png"
+        />
+      </Link>
+      <ul
+        className={`flex flex-row justify-center items-center text-sm gap-4`}
+        style={{ color: "#4b4b4b" }}
+      >
         <ProductsItem name={"عینک طبی"} />
         <ProductsItem name={"عینک کامپیوتر"} />
         <ProductsItem name={"عینک آفتابی"} />
-    </ul>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsList
+export default ProductsList;
