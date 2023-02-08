06179dca710af54236ce762e19daf4d257f89b34
sudo.developer <erfanmashari2@gmail.com>
2023-02-08 16:15:57 +0330

    set getServerSideProps for product page index
    getServerSideProps is set for product page index

--- frontend/public/pages/product/[name]/index.js
+++ frontend/public/pages/product/[name]/index.js
@@ -1,11 +1,31 @@
 import ProductComponent from "../../../components/Product";
 
-const Product = () => {
+import { useDispatch } from "react-redux";
+
+import { changeProductPageInfoFromBackend } from "../../../redux/actions/product";
+
+const Product = ({ product }) => {
+  const dispatch = useDispatch();
+
+  // set product info to redux
+  dispatch(changeProductPageInfoFromBackend(product))
+  
   return (
     <>
       <ProductComponent />
     </>
   );
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER}products/single/${context.params.name}/`);
+  const json = await res.json();
+
+  return {
+    props: {
+      product: json.status === 200 ? json.data.product : {},
+    },
+  };
 };
 
 export default Product;
