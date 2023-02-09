cf7c01ec9d1707b2329b09f1feaae73d1ac13ef3
sudo.developer <erfanmashari2@gmail.com>
2023-02-09 12:54:30 +0330

    Hide layout for login and register page
    layout for login and register page is hidden

--- frontend/public/pages/_app.js
+++ frontend/public/pages/_app.js
@@ -1,20 +1,29 @@
 // import components
 import Layout from "../components/Layout";
 
+import { useRouter } from "next/router";
+
 // import redux tools
 import { wrapper, store } from "../redux/store";
 import { Provider } from "react-redux";
 
 // import styles
 import "../styles/globals.css";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
   return (
     <Provider store={store}>
-      <Layout>
+      {router.pathname === "/login" ||
+      router.pathname === "/register" ? (
         <Component {...pageProps} />
-      </Layout>
+      ) : (
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      )}
     </Provider>
   );
 }
