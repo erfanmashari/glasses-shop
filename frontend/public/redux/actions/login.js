df4fbf82e9bc9676960bc85c43b03fda204d9fcd
sudo.developer <erfanmashari2@gmail.com>
2023-02-10 09:43:55 +0330

    Set reducer for login info
    reducer for login info is set

--- frontend/public/redux/actions/login.js
+++ frontend/public/redux/actions/login.js
@@ -5,3 +5,12 @@ export const changeLoginStatus = (status) => {
         type: "CHANGE_LOGIN_STATUS"
     }
 }
+
+// action for changing login info for user login
+export const changeLoginInfo = (parameter, value) => {
+    return {
+        value,
+        parameter,
+        type: "CHANGE_LOGIN_INFO"
+    }
+}
