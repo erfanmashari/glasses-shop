8ac9c5aecf0b12e8d126aebe999c5563adde7662
sudo.developer <erfanmashari2@gmail.com>
2023-02-10 11:37:34 +0330

    Fix not sending register request
    not sending register request is set

--- frontend/public/redux/reducers/login/loginInfo.js
+++ frontend/public/redux/reducers/login/loginInfo.js
@@ -1,16 +1,31 @@
-const loginInfo = (state = {}, action) => {
-    switch (action.type) {
-        case "CHANGE_LOGIN_INFO":
-            const items = { ...state }
-            items[action.parameter] = action.value;
-            state = items;
-            return state;
-        case "RESET_LOGIN_INFO":
-            state = {};
-            return state;
-        default:
-            return state;
-    }
-}
+const loginInfo = (
+  state = {
+    phoneNumber: "",
+    code: "",
+    firstName: "",
+    lastName: "",
+    gender: "مرد",
+  },
+  action
+) => {
+  switch (action.type) {
+    case "CHANGE_LOGIN_INFO":
+      const items = { ...state };
+      items[action.parameter] = action.value;
+      state = items;
+      return state;
+    case "RESET_LOGIN_INFO":
+      state = {
+        phoneNumber: "",
+        code: "",
+        firstName: "",
+        lastName: "",
+        gender: "مرد",
+      };
+      return state;
+    default:
+      return state;
+  }
+};
 
 export default loginInfo;
