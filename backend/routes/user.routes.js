0c912a3df7da69c285e6c4e808f6ab88665300ef
sudo.developer <erfanmashari2@gmail.com>
2023-02-11 12:13:37 +0330

    Set edit personal info route in user routes
    editting personal info route in user routes is set

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,6 +4,7 @@ const userController = require("../controllers/user.controller");
 const userRouter = express.Router();
 
 userRouter.get("/", userController.user_index);
+userRouter.get("/edit", userController.edit_personal_info);
 userRouter.get("/:phoneNumber", userController.user_index_phone_number);
 
 module.exports = userRouter;
