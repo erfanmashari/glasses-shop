52652a74235d67e8ffbe6a5f592617f67907d8a8
sudo.developer <erfanmashari2@gmail.com>
2023-02-11 18:00:12 +0330

    Complete add address controller
    add address controller is completed

--- backend/controllers/address.controller.js
+++ backend/controllers/address.controller.js
@@ -1,3 +1,4 @@
+const User = require("../models/user.model");
 const Address = require("../models/address.model");
 const { jsonResponse, checkDataExist } = require("../functions");
 
@@ -14,8 +15,57 @@ const address_index = async (req, res) => {
 };
 
 // create new addrss for user
-const add_address = (req, res) => {
-  res.json(jsonResponse(200, { message: "آدرس جدید با موفقیت افزوده شد!" }));
+const add_address = async (req, res) => {
+  const body = req.body;
+    console.log("body: ", body)
+  if (
+    !checkDataExist(
+      body,
+      ["userId", "province", "city", "plaque", "postalAddress", "postalCode"],
+      res
+    ) ||
+    !checkDataExist(
+      body.receiverSpecifications,
+      ["firstName", "phoneNumber", "lastName"],
+      res
+    ) ||
+    !checkPhoneNumber(body.receiverSpecifications.phoneNumber, res)
+  ) {
+    return null;
+  }
+
+  Address.create(body).then(result => {
+    User.findOne({ _id: body.userId }, (err, user) => {
+        if (user) {
+            // The below two lines will add the newly saved address's 
+            // ObjectID to the the User's addresses array field
+            user.addresses.push(result);
+            user.save();
+            res.json(jsonResponse(200, { message: "آدرس جدید با موفقیت افزوده شد!" }));
+        }
+    });
+  })
+  .catch((error) => {
+    res.status(500).json({ error });
+  });
+};
+
+const checkPhoneNumber = (phoneNumber, res) => {
+  if (phoneNumber.length !== 11) {
+    res.json(
+      jsonResponse(406, {
+        message: "شماره همراه باید شامل 11 کاراکتر باشد!",
+      })
+    );
+  } else if (phoneNumber.charAt(0) !== "0") {
+    res.json(
+      jsonResponse(406, {
+        message: "شماره همراه باید با 0 شروع شود!",
+      })
+    );
+  } else {
+    return true;
+  }
 };
 
 module.exports = { address_index, add_address };
--- backend/models/address.model.js
+++ backend/models/address.model.js
@@ -3,16 +3,17 @@ const Schema = mongoose.Schema;
 
 const addressSchema = new Schema(
   {
+    userId: { type: Schema.Types.ObjectId, required: true },
     province: { type: String, maxLength: 30, required: true },
     city: { type: String, maxLength: 30, required: true },
     plaque: { type: String, maxLength: 10, required: true },
     postalAddress: { type: String, required: true },
     postalCode: { type: String, maxLength: 12, required: true },
-    isReceiver: { type: Boolean },
+    isMeReceiver: { type: Boolean },
     receiverSpecifications: {
-        firstName: { type: String, maxLength: 50 },
-        lastName: { type: String, maxLength: 50 },
-        phoneNumber: { type: String, maxLength: 15 },
+      firstName: { type: String, maxLength: 50 },
+      lastName: { type: String, maxLength: 50 },
+      phoneNumber: { type: String, maxLength: 15 },
     },
     unit: { type: String, maxLength: 5 },
   },
