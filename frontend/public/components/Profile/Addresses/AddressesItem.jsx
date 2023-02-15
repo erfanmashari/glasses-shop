32cfbf97f3c81ebb672b1ab69abe7f5f2d559a27
sudo.developer <erfanmashari2@gmail.com>
2023-02-15 18:55:40 +0330

    Showing addresses list is set in addresses page
    set to show addresses list in addresses page

--- frontend/public/components/Profile/Addresses/AddressesItem.jsx
+++ frontend/public/components/Profile/Addresses/AddressesItem.jsx
@@ -1,11 +1,11 @@
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const AddressesItem = () => {
+const AddressesItem = ({ postalAddress, number }) => {
   return (
     <tr className="text-center text-slate-500 border-b-2 border-gray-200">
-      <td className="py-4">1</td>
-      <td className="text-stone-800 py-4">نشانی پستی تستی</td>
+      <td className="py-4">{number}</td>
+      <td className="text-stone-800 py-4">{postalAddress}</td>
       <td className="py-4 cursor-pointer">
         <EditIcon className="w-5 h-5 block mx-auto" />
       </td>
--- frontend/public/components/Profile/Addresses/AddressesList.jsx
+++ frontend/public/components/Profile/Addresses/AddressesList.jsx
@@ -1,24 +1,35 @@
-import AddressesItem from "./AddressesItem"
+import AddressesItem from "./AddressesItem";
+
+import { useSelector } from "react-redux";
 
 const AddressesList = () => {
+  // get personal info from reduc/reducer/profile/personalInfo.js
+  const personalInfo = useSelector((state) => state.personalInfo);
+
   return (
     <>
-        <h3 className='w-full text-xl font-bold text-stone-800'>آدرس ها</h3>
-        <table className='w-full'>
-            <thead>
-                <tr className='w-full text-stone-800 border-b-2 border-gray-200'>
-                    <th className='w-1/12 py-4'>ردیف</th>
-                    <th className='w-9/12 py-4'>نشانی پستی</th>
-                    <th className='w-1/12 py-4'>ویرایش</th>
-                    <th className='w-1/12 py-4'>حذف</th>
-                </tr>
-            </thead>
-            <tbody>
-                <AddressesItem />
-            </tbody>
-        </table>
+      <h3 className="w-full text-xl font-bold text-stone-800">آدرس ها</h3>
+      <table className="w-full">
+        <thead>
+          <tr className="w-full text-stone-800 border-b-2 border-gray-200">
+            <th className="w-1/12 py-4">ردیف</th>
+            <th className="w-9/12 py-4">نشانی پستی</th>
+            <th className="w-1/12 py-4">ویرایش</th>
+            <th className="w-1/12 py-4">حذف</th>
+          </tr>
+        </thead>
+        <tbody>
+          {personalInfo.addresses && personalInfo.addresses.map((address, index) => (
+            <AddressesItem
+              key={index}
+              postalAddress={address.postalAddress}
+              number={index + 1}
+            />
+          ))}
+        </tbody>
+      </table>
     </>
-  )
-}
+  );
+};
 
-export default AddressesList
+export default AddressesList;
