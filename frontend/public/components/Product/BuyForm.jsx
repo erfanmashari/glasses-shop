6947d9f83d39aaea9df62635712f6a941821f20a
sudo.developer <erfanmashari2@gmail.com>
2023-02-17 23:20:15 +0330

    Fix not updating cart when a product added
    not updating cart when a product added is fixed

--- frontend/public/components/Product/BuyForm.jsx
+++ frontend/public/components/Product/BuyForm.jsx
@@ -6,7 +6,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { changeSelectedProductProperties } from "../../redux/actions/product";
 
 import axiosApp from "../../utils/axiosConfig";
-import { checkFetchResponse, toastAlert } from "../../functions";
+import { checkFetchResponse, toastAlert, getUserInfo } from "../../functions";
 
 import { Tooltip } from "flowbite-react";
 
@@ -39,13 +39,14 @@ const BuyForm = () => {
     const selectedProductInfo = { ...selectedProduct };
     selectedProductInfo.userId = personalInfo._id;
     if (!selectedProductInfo.frameColor) {
-      toastAlert("رنگ محصول را انتخاب کنید!", "error")
+      toastAlert("رنگ محصول را انتخاب کنید!", "error");
     } else if (!selectedProductInfo.size) {
-      toastAlert("سایز محصول را انتخاب کنید!", "error")
+      toastAlert("سایز محصول را انتخاب کنید!", "error");
     } else {
       axiosApp.post("cart", selectedProductInfo).then((response) => {
         const res = checkFetchResponse(response);
         if (res.ok) {
+          getUserInfo();
           toastAlert(res.data.message, "success");
         } else {
           toastAlert(res.message, "error");
