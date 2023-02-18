e00100b6b299058b502447d94c9569e470f24386
sudo.developer <erfanmashari2@gmail.com>
2023-02-18 22:20:14 +0330

    complete add notification controller
    add notification controller is completed

---
+++ backend/controllers/notification.controller.js
@@ -0,0 +1,47 @@
+const Notification = require("../models/notification.model");
+const User = require("../models/user.model");
+const { jsonResponse, checkDataExist } = require("../functions");
+
+const add_notification = async (req, res) => {
+  const body = req.body;
+
+  if (!checkDataExist(body, ["userId", "title"], res)) {
+    return null;
+  }
+
+  const checkUserStatus = await checkUser(body.userId, res);
+  if (!checkUserStatus) {
+    return null;
+  }
+
+  Notification.create(body)
+    .then((result) => {
+      User.findOne({ _id: body.userId }, (err, user) => {
+        if (user) {
+          // The below two lines will add the newly saved notification's
+          // ObjectID to the the User's notifications array field
+          user.notifications.push(result._id);
+          user.save();
+          res.json(
+            jsonResponse(200, { message: "پیغام جدید با موفقیت افزوده شد!" })
+          );
+        }
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({ error });
+    });
+};
+
+// check if user is real or not
+const checkUser = async (userId, res) => {
+  const user = await User.findOne({ _id: userId });
+
+  if (!user) {
+    res.json(jsonResponse(406, { message: "کاربر مورد نظر معتبر نمی باشد!" }));
+  }
+
+  return user ? true : false;
+};
+
+module.exports = { add_notification };
