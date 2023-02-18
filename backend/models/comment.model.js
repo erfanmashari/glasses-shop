b22322766bf2004f1e5ea76b1479283835c81ac6
sudo.developer <erfanmashari2@gmail.com>
2023-02-18 10:32:20 +0330

    Complete comment model
    comment model is completed

---
+++ backend/models/comment.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    userId: { type: Schema.Types.ObjectId, required: true },
+    productId: { type: Schema.Types.ObjectId, required: true },
+    title: { type: String, maxLength: 80, required: true },
+    description: { type: String, required: true },
+    status: { type: String, required: true }, // در حال بررسی - تایید شده- رد شده
+    stars: { type: Number, required: true }, // 1 - 2 - 3 - 4 - 5
+    positivePoints: { type: [String] },
+    negativePoints: { type: [String] },
+  },
+  { timestamps: true, collection: "comments" }
+);
