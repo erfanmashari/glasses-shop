81731b23b635d967b668584d85272ee6f6a67df0
sudo.developer <erfanmashari2@gmail.com>
2023-02-24 13:59:52 +0330

    Add a field to order model
    a field is added to order model

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -10,6 +10,7 @@ const orderSchema = new Schema(
     status: { type: String, required: true }, // جاری - بسته بندی - ارسال شده - مرجوع شده - لغو شده
     paymentMethod: { type: String, required: true }, // زرین پال
     sendingMethod: { type: String, required: true }, // پست پیشتاز
+    transaction: { type: Schema.Types.ObjectId },
     products: { type: [Object], required: true },
   },
   { timestamps: true, collection: "orders" }
