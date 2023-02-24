6f751742eaa6712b0125b91c7b6ac5886786de0e
sudo.developer <erfanmashari2@gmail.com>
2023-02-24 14:03:36 +0330

    Set get single order routes
    getting single order routes is set

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -4,5 +4,6 @@ const orderController = require("../controllers/order.controller")
 const orderRoutes = express.Router();
 
 orderRoutes.post("/", orderController.add_order);
+orderRoutes.get("/:id", orderController.order_single);
 
 module.exports = orderRoutes;
