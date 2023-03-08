0172e35b224362bc91354a90ce616bcf854ebf9c
sudo.developer <erfanmashari2@gmail.com>
2023-03-08 12:52:42 +0330

    Set password api in password page

--- frontend/public/components/Profile/Password/FormInput.jsx
+++ frontend/public/components/Profile/Password/FormInput.jsx
@@ -13,7 +13,7 @@ const FormInput = ({
       <input
         value={formFields[parameter]}
         onChange={(e) => changeInputValue(parameter, e.target.value)}
-        type={type ? type : "text"}
+        type={"password"}
         required={true}
         placeholder={placeholder}
         className={`w-full px-5 py-2.5 border-none outline-none rounded-3xl shadow-sm shadow-stone-400`}
--- frontend/public/components/Profile/Password/SetPassword.jsx
+++ frontend/public/components/Profile/Password/SetPassword.jsx
@@ -4,6 +4,9 @@ import React, { useState } from "react";
 
 import { useSelector } from "react-redux";
 
+import axiosApp from "../../../utils/axiosApp";
+import { checkFetchResponse, toastAlert } from "../../../functions";
+
 const SetPassword = () => {
   // get personal info from reduc/reducer/profile/personalInfo.js
   const personalInfo = useSelector((state) => state.personalInfo);
@@ -20,12 +23,35 @@ const SetPassword = () => {
     setFormFields(items);
   };
 
+  // send password request to backend
+  const setPassword = (e) => {
+    e.preventDefault();
+
+    const reqData = { ...formFields, userId: personalInfo._id };
+    axiosApp.post("password", reqData).then((response) => {
+      const res = checkFetchResponse(response);
+
+      if (res.ok) {
+        toastAlert(res.data.message, "success");
+        setFormFields({
+          password: "",
+          confirmPassword: "",
+        });
+      } else {
+        toastAlert(res.message, "error");
+      }
+    });
+  };
+
   return (
-    <div className="w-9/12 flex flex-col justify-center self items-center border-t-2 border-blue-600 shadow-md rounded-lg gap-8 p-8">
+    <div className="w-9/12 flex flex-col justify-center self items-center border-t-2 border-blue-600 shadow-md rounded-lg gap-8 px-12 py-16">
       <h3 className="w-full text-xl font-bold text-stone-800">
         {personalInfo.password ? "تغییر" : "تعیین"} رمز عبور
       </h3>
-      <form className="w-full flex flex-row items-end gap-6">
+      <form
+        onSubmit={setPassword}
+        className="w-full flex flex-row items-end gap-6"
+      >
         <FormInput
           width={"w-5/12"}
           label={"رمز عبور"}
@@ -54,6 +80,13 @@ const SetPassword = () => {
           {personalInfo.password ? "تغییر" : "تعیین"} رمز عبور{" "}
         </button>
       </form>
+      <ul className="w-full flex flex-col list-disc gap-2 px-5">
+        <li>رمز عبور باید حداقل 8 کاراکتر باشد!</li>
+        <li>رمز عبور باید شامل حروف کوچک انگلیسی باشد!</li>
+        <li>رمز عبور باید شامل حروف بزرگ انگلیسی باشد!</li>
+        <li>رمز عبور باید شامل اعداد باشد!</li>
+        <li>رمز عبور باید شامل کاراکتر خاص($@#&!) باشد!</li>
+      </ul>
     </div>
   );
 };
