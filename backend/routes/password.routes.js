c866cefedb2fa4429d4fb1474535c7e5343fa853
sudo.developer <erfanmashari2@gmail.com>
2023-03-08 11:35:49 +0330

    Set change password routes

--- backend/routes/password.routes.js
+++ backend/routes/password.routes.js
@@ -4,5 +4,7 @@ const passwordController = require("../controllers/password.controller")
 const passwordRoutes = express.Router();
 
 passwordRoutes.post("/", passwordController.set_password);
+passwordRoutes.post("/change", passwordController.change_password);
+passwordRoutes.post("/confirm", passwordController.confirm_code);
 
 module.exports = passwordRoutes;
