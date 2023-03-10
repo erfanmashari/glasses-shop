d9a46d4f504fe777db9a896dc0f5b75ad9f5a4bb
sudo.developer <erfanmashari2@gmail.com>
2023-03-10 15:01:13 +0330

    Complete notifications page APIs

--- frontend/public/components/Profile/Notifications/NotificationsItem.jsx
+++ frontend/public/components/Profile/Notifications/NotificationsItem.jsx
@@ -1,14 +1,87 @@
-import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { useSelector, useDispatch } from "react-redux";
+
+import axiosApp from "../../../utils/axiosApp";
+import {
+  checkFetchResponse,
+  toastAlert,
+  getTokenFromCookie,
+  getUserInfo,
+} from "../../../functions";
+
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import DoneRoundedIcon from "@mui/icons-material/DoneRounded";
+import DoneAllRoundedIcon from "@mui/icons-material/DoneAllRounded";
 
 const NotificationsItem = ({ notification, index }) => {
+  const dispatch = useDispatch();
+
+  // get personal info from reduc/reducer/profile/personalInfo.js
+  const personalInfo = useSelector((state) => state.personalInfo);
+
+  // send edit address info request to backend
+  const editAddress = (e) => {
+    e.preventDefault();
+
+    const reqData = {
+      id: notification._id,
+    };
+    axiosApp
+      .put("notifications/seen", reqData, {
+        headers: { Authorization: getTokenFromCookie() },
+      })
+      .then((response) => {
+        const res = checkFetchResponse(response);
+
+        if (res.ok) {
+          getUserInfo(dispatch);
+          toastAlert(res.data.message, "success");
+        } else {
+          toastAlert(res.message, "error");
+        }
+      });
+  };
+
+  // send delete address request to backend
+  const deleteAddress = () => {
+    axiosApp
+      .delete("notifications", {
+        headers: {
+          Authorization: getTokenFromCookie(),
+        },
+        data: {
+          id: notification._id,
+          user: personalInfo._id,
+        },
+      })
+      .then((response) => {
+        const res = checkFetchResponse(response);
+        if (res.ok) {
+          getUserInfo(dispatch);
+          toastAlert(res.data.message, "success");
+        } else {
+          toastAlert(res.message, "error");
+        }
+      });
+  };
   return (
     <tr className="text-center text-slate-500 border-b-2 border-gray-200">
       <td className="py-4">{index + 1}</td>
       <td className="text-stone-800 py-4">{notification.title}</td>
-      <td className="text-blue-600 py-4">{notification.createdAt.split("T")[0]}</td>
-      <td className="py-4">{notification.seen ? "دیده شده" : "دیده نشده"}</td>
+      <td className="text-blue-600 py-4">
+        {notification.createdAt.split("T")[0]}
+      </td>
+      <td className="py-4">
+        {notification.isSeen ? (
+          <DoneAllRoundedIcon className="text-yellow-400" />
+        ) : (
+          <DoneRoundedIcon className="cursor-pointer" onClick={editAddress} />
+        )}
+      </td>
       <td className="py-4 cursor-pointer">
-        <DeleteOutlineOutlinedIcon className="text-red-600 block mx-auto" />
+        <DeleteOutlineOutlinedIcon
+          className="text-red-600 block mx-auto"
+          onClick={deleteAddress}
+        />
       </td>
     </tr>
   );
--- frontend/public/components/Profile/Notifications/NotificationsList.jsx
+++ frontend/public/components/Profile/Notifications/NotificationsList.jsx
@@ -9,14 +9,14 @@ const NotificationsList = () => {
 
   return (
     <div className="w-9/12 flex flex-col justify-center items-center border-2 border-gray-200 rounded-lg gap-4 p-6">
-      <h3 className="w-full text-xl font-bold text-stone-800">نظرات</h3>
+      <h3 className="w-full text-xl font-bold text-stone-800">پیغام ها</h3>
       <table className="w-full">
         <thead>
           <tr className="w-full text-stone-800 border-b-2 border-gray-200">
             <th className="w-1/12 py-4">ردیف</th>
             <th className="w-5/12 py-4">عنوان نظر</th>
             <th className="w-3/12 py-4">تاریخ ثبت</th>
-            <th className="w-2/12 py-4">وضعیت</th>
+            <th className="w-2/12 py-4">دیده شده</th>
             <th className="w-1/12 py-4">حذف</th>
           </tr>
         </thead>
